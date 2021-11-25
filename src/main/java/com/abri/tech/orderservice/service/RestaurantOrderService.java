158b6ea6d374ab1fbbdb7160507194eafe281667
amitdatta <the.amisoft@gmail.com>
2021-11-25 08:16:11 +0530

    Restaurant order saved

--- src/main/java/com/abri/tech/orderservice/service/RestaurantOrderService.java
+++ src/main/java/com/abri/tech/orderservice/service/RestaurantOrderService.java
@@ -6,6 +6,7 @@ import com.abri.tech.orderservice.repo.RestaurantOrderRepo;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 @AllArgsConstructor
@@ -20,17 +21,11 @@ public class RestaurantOrderService {
 
         var order = new Order();
         //RestaurantOrder.java  ->  Order.java
-        order.setCustomerName(restaurantOrder.getCustomerName());
-        order.setMenuName(restaurantOrder.getMenuName());
-
-        //BeanUtils.copyProperties(restaurantOrder,order);
+        BeanUtils.copyProperties(restaurantOrder,order);
         var savedOrder = restaurantOrderRepo.save(order);
 
         var savedOrderDto = new RestaurantOrder();
-        savedOrderDto.setCustomerName(savedOrder.getCustomerName());
-        savedOrderDto.setMenuName(savedOrder.getMenuName());
-
-        //BeanUtils.copyProperties(savedOrder,savedOrderDto);
+        BeanUtils.copyProperties(savedOrder,savedOrderDto);
         return savedOrderDto;
     }
 }
